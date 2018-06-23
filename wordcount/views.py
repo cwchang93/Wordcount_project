dc8896d377bc13b01142c1ba5ea5d080fc0e8124
張峻維 <cwchang@zhangjunweide-MacBook-Air.local>
2018-06-23 09:31:05 +0800

    First Wordcount project

---
+++ wordcount/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    return render(request, 'home.html')
+def count(request):
+    fulltext = request.GET['fulltext']
+    #print(fulltext)
+    wordlist = fulltext.split()
+    worddict = dict()
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+#    sortword = sorted(worddict.items(), key=operator.itemgetter(1), reverse = True)
+
+    return render(request, 'count.html', {'fulltext': fulltext, \
+            'count': len(wordlist), 'worddict':worddict.items()})
+def about(request):
+    return render(request, 'about.html')
+    
